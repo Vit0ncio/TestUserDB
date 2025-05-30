887dfd3d2ae18718f7c9fd176015ea8cbb1122c1
Vitor Luis <vitorluisgomesvieira0@gmail.com>
2025-05-30 10:16:28 -0300

    Add: Database (db) folder with the database file

--- src/br/testuserdb/Main.java
+++ src/br/testuserdb/Main.java
@@ -1,12 +1,10 @@
 package br.testuserdb;
 
-import br.testuserdb.dao.ConnectDAO;
+import br.testuserdb.dao.DataDAO;
 
 public class Main {
     public static void main(String[] args) {
-        ConnectDAO connDAO = new ConnectDAO();
-
-        connDAO.connectDB();
-        connDAO.disconnectDB();
+        DataDAO dataDAO = new DataDAO();
+        dataDAO.createDB();
     }
 }
