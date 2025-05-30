32ec9a22448e05cdb2b2932f84188e0753a5d1f2
Vitor Luis <vitorluisgomesvieira0@gmail.com>
2025-05-30 09:09:49 -0300

    Connection/Disconnection with database

--- src/br/testuserdb/dao/DataDAO.java
+++ src/br/testuserdb/dao/DataDAO.java
@@ -1,4 +1,38 @@
 package br.testuserdb.dao;
 
+import java.sql.*;
+
 public class DataDAO {
+    Connection conn = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+
+    private void connectDB() {
+        try {
+            conn = DriverManager.getConnection("jdbc:sqlite:testuser.db", "root", "");
+            System.out.println("Connection successful.");
+        } catch (SQLException sqle) {
+            System.out.println(sqle.getMessage());
+        }
+    }
+
+    private void disconnectDB() {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException sqle) { }
+        }
+
+        if (ps != null) {
+            try {
+                ps.close();
+            } catch (SQLException sqle) { }
+        }
+
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) { }
+        }
+    }
 }
