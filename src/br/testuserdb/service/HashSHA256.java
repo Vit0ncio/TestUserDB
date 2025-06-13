5754ed83ab949485de1e9b4e51d9936876dfff93
Vitor Luis <vitorluisgomesvieira0@gmail.com>
2025-06-13 09:18:52 -0300

    Add: Hash to passwords

--- src/br/testuserdb/service/HashSHA256.java
+++ src/br/testuserdb/service/HashSHA256.java
@@ -1,4 +1,22 @@
 package br.testuserdb.service;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class HashSHA256 {
+    public static String hashPassword(String password) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hashedBytes = md.digest(password.getBytes());
+            StringBuilder sb = new StringBuilder();
+
+            for (byte b : hashedBytes) {
+                sb.append(String.format("%02x", b));
+            }
+
+            return sb.toString();
+        } catch (NoSuchAlgorithmException nsae) {
+            throw new RuntimeException("Error in hashing", nsae);
+        }
+    }
 }
